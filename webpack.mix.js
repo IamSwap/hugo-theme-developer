3bd27429e19082d66c3e2151c19266a4dd7742fc
Swapnil Bhavsar <swapnil@swapnil.co>
2018-08-18 23:30:58 +0530

    Added assets setup

---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+let mix = require('laravel-mix');
+let tailwindcss = require('tailwindcss');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+mix.sass('src/sass/style.scss', 'static/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('./tailwind.js') ],
+    });
